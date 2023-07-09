3445624b4295049d900b36d0f8955d33599eb24d
winter <92315795+winterspriggan@users.noreply.github.com>
2023-07-09 17:48:01 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        if(a%4==0 && a%100 != 0) {
+            System.out.print(1);
+        } else if(a%400 == 0) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+	}
+
+}
