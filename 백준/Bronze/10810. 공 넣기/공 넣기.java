e1d14a241afa587fdab85252c2b9a7b6458fb50c
winter <92315795+winterspriggan@users.noreply.github.com>
2023-07-12 17:06:05 +0900

    [Bronze III] Title: 공 넣기, Time: 152 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
@@ -11,8 +10,8 @@ public class Main {
         String[] t2 = t.split(" ");
         int[] basket = new int[Integer.parseInt(t2[0])];
         String[] ball  = new String[Integer.parseInt(t2[1])];
-//        ArrayList<Integer> temp = new ArrayList<>();
         ArrayList<ArrayList<Integer>> temp = new ArrayList<>();
+
         for(int i=0; i<ball.length; i++) {
         ArrayList<Integer> temps = new ArrayList<>();
             ball[i] = bf.readLine();
@@ -23,13 +22,12 @@ public class Main {
             }
             temp.add(temps);
         }
+
         for(int i=0; i<ball.length; i++) {
             for(int j=temp.get(i).get(0)-1; j<temp.get(i).get(1); j++) {
                 basket[j] = temp.get(i).get(2);
             }
         }
-        for (int i=0; i<basket.length; i++) {
-            System.out.print(basket[i]+" ");
-        }
+        for (int i=0; i<basket.length; i++) System.out.print(basket[i]+" ");
     }
 }
