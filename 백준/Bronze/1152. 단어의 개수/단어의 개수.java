de8b96565a363675a0b7cf7b2161cd248e94c715
winter <92315795+winterspriggan@users.noreply.github.com>
2023-07-17 17:04:29 +0900

    [Bronze II] Title: 단어의 개수, Time: 304 ms, Memory: 26384 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        ArrayList<String> t = new ArrayList<>();
+        while(st.hasMoreElements()) {
+            t.add(st.nextToken());
+        }
+        System.out.println(t.size());
+
+    }
+}
