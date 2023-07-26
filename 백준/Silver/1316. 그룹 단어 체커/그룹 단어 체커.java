a7a0f0c3ef7c92f2a50bda43ace0afab4b80b230
winter <92315795+winterspriggan@users.noreply.github.com>
2023-07-26 17:04:48 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 152 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -12,19 +11,13 @@ public class Main {
         for(int i=0; i<numb; i++) {
             String word = bf.readLine();
             ArrayList<Character> temp = new ArrayList<>();
-            for(int j=0; j<word.length(); j++) {
-                        temp.add(word.charAt(j));
-            }
+            for(int j=0; j<word.length(); j++) temp.add(word.charAt(j));
             boolean isGroup = true;
             for(int j=0; j<temp.size(); j++) {
                 char t = temp.get(j);
-                for(int k =temp.indexOf(t); k<temp.lastIndexOf(t); k++) {
-                    if(!temp.get(k).equals(t)) isGroup =false;
-                }
+                for(int k =temp.indexOf(t); k<temp.lastIndexOf(t); k++) if(!temp.get(k).equals(t)) isGroup =false;
             }
-            if(isGroup) {
-                count ++;
-            }
+            if(isGroup) count ++;
         }
         System.out.print(count);
     }
