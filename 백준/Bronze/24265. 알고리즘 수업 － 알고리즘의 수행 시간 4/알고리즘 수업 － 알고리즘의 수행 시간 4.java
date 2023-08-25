b41458ab6d92ca37e46ef3576e66dfd2e1a27422
winter <92315795+winterspriggan@users.noreply.github.com>
2023-08-25 19:52:21 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 224 ms, Memory: 18280 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.print((n-1)*n/2+"\n"+2);
+    }
+}
