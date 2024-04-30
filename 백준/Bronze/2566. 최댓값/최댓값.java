de3a594753c9f3dcb653bbd13d5c38eb035577fa
winter <92315795+winterspriggan@users.noreply.github.com>
2024-04-30 13:03:04 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,24 +1,26 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int mat[][] = new int[9][9];
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[9][9];
         int max = 0;
-        int a =0; int b = 0;
-        for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                mat[i][j] =sc.nextInt();
-                if(max <= mat[i][j]) {
-                    max = mat[i][j];
-                    a =i+1;
-                    b =j+1;
+        int x = 0, y = 0;
+        for (int i = 0; i < 9; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+                if (arr[i][j] >= max) {
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
                 }
             }
         }
-        sc.close();
         System.out.println(max);
-        System.out.print( a+" " +b);
+        System.out.println(x + " " + y);
+        br.close();
     }
 }
