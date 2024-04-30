e49e3d7dc525b66531dae3a20a3fff996bb4817a
winter <92315795+winterspriggan@users.noreply.github.com>
2024-04-30 13:06:18 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] arr = new int[9][9];
-        int max = 0;
-        int x = 0, y = 0;
-        for (int i = 0; i < 9; i++) {
-            String[] input = br.readLine().split(" ");
-            for (int j = 0; j < 9; j++) {
-                arr[i][j] = Integer.parseInt(input[j]);
-                if (arr[i][j] >= max) {
-                    max = arr[i][j];
-                    x = i + 1;
-                    y = j + 1;
-                }
-            }
-        }
-        System.out.println(max);
-        System.out.println(x + " " + y);
-        br.close();
-    }
-}
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ public class Main {
+     public static void main(String[] args) throws IOException {
+         int mat[][] = new int[9][9];
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int max = 0;
+         int a =0; int b = 0;
+ for(int i=0; i<9; i++) {
+             String[] input = br.readLine().split(" ");
+             for(int j=0; j<9; j++) {
+                 mat[i][j] = Integer.parseInt(input[j]);
+ if(max <= mat[i][j]) {
+max = mat[i][j];
+ a =i+1;
+ b =j+1;
+                 }
+             }
+         }
+         br.close();
+         System.out.println(max);
+         System.out.print( a+" " +b);
+     }
+ }
